e3d3b36438ab9362bf59c979e48d3c232ad05c7a
Havilash Sivaratnam <havilash2005@gmail.com>
2023-08-20 10:54:11 +0200

    min_subarray_sum

---
+++ problems/utils.py
@@ -0,0 +1,13 @@
+from functools import wraps
+import time
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__!r} executed in {(total_time):.4f}s')
+        return result
+    return wrapper
